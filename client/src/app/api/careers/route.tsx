9a38110bbb975e14184272b72833c6c39602ab5a
Cocofredo <3953441@gmail.com>
2023-10-04 20:40:19 -0300

    testeando mas y mas

---
+++ client/src/app/api/careers/route.tsx
@@ -0,0 +1,12 @@
+import { clientRequestHandler } from '@/utils/ApiFunctions';
+import { ApiCommunicator } from '@/services/ApiCommunicator';
+
+export async function GET(request: Request) {
+  return clientRequestHandler({
+    req: request,
+    handlerNeedsBody: false,
+    apiHandler: async () => {
+      return await ApiCommunicator.getCareers();
+    },
+  });
+}
